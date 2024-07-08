031596298c047d1b851ea58d7d23b46f3908a472
Amir1453 <amir.1.sabbag@gmail.com>
2024-07-08 18:19:57 +0200

    Initial commit for cpp-base

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "template.hpp"
+
+int main() {
+  hello_world();
+  return 0;
+}
---
+++ src/template.cpp
@@ -0,0 +1,6 @@
+#include "template.hpp"
+#include <cstdio>
+
+void hello_world() {
+  puts("Hello world!");
+}
